53ffd180c3c934d4e2bebb36277bfb04b7666f66
Sarunas Verbus <sarunas@verbus.lt>
2020-09-04 18:44:13 +0300

    Initial Project Add

---
+++ src/main/java/lt/verbus/verbuspetclinic/VerbusPetClinicApplication.java
@@ -0,0 +1,13 @@
+package lt.verbus.verbuspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbusPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerbusPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/verbus/verbuspetclinic/VerbusPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.verbus.verbuspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbusPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
