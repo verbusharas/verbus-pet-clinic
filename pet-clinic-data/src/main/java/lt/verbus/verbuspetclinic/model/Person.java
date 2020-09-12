1b84a1d1edf61c836f2287cc5a913aa99d6fd17e
Sarunas Verbus <sarunas@verbus.lt>
2020-09-12 15:18:56 +0300

    adding base entity. Closes #10

--- pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package lt.verbus.verbuspetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package lt.verbus.verbuspetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
