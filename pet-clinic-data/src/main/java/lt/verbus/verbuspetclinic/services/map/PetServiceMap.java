c6a77c252e400c16d64a94408173efe0fd390c2a
Sarunas Verbus <sarunas@verbus.lt>
2020-09-13 13:47:55 +0300

    adding bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package lt.verbus.verbuspetclinic.services.map;
 
 import lt.verbus.verbuspetclinic.model.Pet;
-import lt.verbus.verbuspetclinic.services.CrudService;
+import lt.verbus.verbuspetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package lt.verbus.verbuspetclinic.services.map;
 
 import lt.verbus.verbuspetclinic.model.Vet;
-import lt.verbus.verbuspetclinic.services.CrudService;
+import lt.verbus.verbuspetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
