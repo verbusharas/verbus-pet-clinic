3f4a005e5e4695843e071cc740e092123c13a40d
Sarunas Verbus <sarunas@verbus.lt>
2020-09-19 20:50:09 +0300

    Added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package lt.verbus.verbuspetclinic.services;
+
+import lt.verbus.verbuspetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/lt/verbus/verbuspetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package lt.verbus.verbuspetclinic.services.map;
+
+import lt.verbus.verbuspetclinic.model.Speciality;
+import lt.verbus.verbuspetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
