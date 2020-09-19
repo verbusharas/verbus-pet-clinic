a508b5cf631254761dec2d62ed7c3ac699c19c3e
Sarunas Verbus <sarunas@verbus.lt>
2020-09-19 20:13:15 +0300

    Added PetTypes to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/lt/verbus/verbuspetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/lt/verbus/verbuspetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package lt.verbus.verbuspetclinic.bootstrap;
 
 import lt.verbus.verbuspetclinic.model.Owner;
+import lt.verbus.verbuspetclinic.model.PetType;
 import lt.verbus.verbuspetclinic.model.Vet;
 import lt.verbus.verbuspetclinic.services.OwnerService;
+import lt.verbus.verbuspetclinic.services.PetTypeService;
 import lt.verbus.verbuspetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
